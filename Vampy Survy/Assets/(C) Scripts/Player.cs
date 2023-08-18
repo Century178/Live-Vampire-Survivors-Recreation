f3bfb5727c388b4efbf1576ecb752a38a5e0bed3
Century178 <mrothenberger0918@gmail.com>
2023-08-17 20:34:00 -0700

    Player animations added
    There are now two animations: Player Idle, and Player Move.
    
    I also renamed the folders.

--- Vampy Survy/Assets/(C) Scripts/Player.cs
+++ Vampy Survy/Assets/(C) Scripts/Player.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed;
+    private float x, y;
+
     public float attackSpeed;
     public float attackPower;
     public float attackRange;
     public float health;
 
-    private float x, y;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Animator am;
+    private bool isMoving;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        am = GetComponent<Animator>();
     }
 
     private void Update()
     {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(x) > 0.1f || Mathf.Abs(y) > 0.1f) isMoving = true;
+        else isMoving = false;
+        am.SetBool("isMoving", isMoving);
 
         if (x < 0) sr.flipX = true;
         else if (x > 0) sr.flipX = false;
