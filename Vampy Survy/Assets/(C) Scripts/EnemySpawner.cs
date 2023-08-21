20fddda3c47c2e629f27d2925395cf8b2e63aef3
Century178 <mrothenberger0918@gmail.com>
2023-08-20 19:15:03 -0700

    Enemy spawning done
    We now have an enemy spawner that spawns enemies every fixed rate and also ramps up in speed.
    
    I also adjusted the hitboxes on the player and enemies to make it fairer.

---
+++ Vampy Survy/Assets/(C) Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private Vector3[] spawnPoint;
+    [SerializeField] private Transform player;
+
+    private float spawnTime;
+    [SerializeField] private float spawnRate;
+    [SerializeField] private float spawnRateMin;
+    [SerializeField] private float rampUp;
+
+    private void Update()
+    {
+        transform.position = player.position;
+
+        spawnTime -= Time.deltaTime;
+
+        if (spawnTime <= 0)
+        {
+            spawnTime = spawnRate;
+            if (spawnRate >= spawnRateMin) spawnRate -= rampUp;
+
+            for (int i = 0; i < spawnPoint.Length; i++)
+            {
+                Instantiate(enemy, transform.position + spawnPoint[i], Quaternion.identity, null);
+            }
+        }
+    }
+}
